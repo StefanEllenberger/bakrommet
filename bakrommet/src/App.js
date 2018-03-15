64311a2252ca604e4bd03266abfaf04bd7e48c34
Stefan Ellenberger <stefan.ellenberger@student.uib.no>
2018-03-15 16:24:47 +0100

    added JSON-parsing to group creation

--- bakrommet/src/App.js
+++ bakrommet/src/App.js
@@ -7,59 +7,48 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      teams: [],
-      teams2: []
+      groupA: [],
+      groupB: [],
+      groupC: [],
+      groupD: []
     }
   }
   //when fetching from API, do it from CWM or CDM
   //check definitions for when they render
   //turn json call into a setState that creates the teamlist
   componentWillMount(){
-    this.setState({teams: [
-      {
-        name:"Brazil",
-        score:3
-      },
-      {
-        name:"Germany",
-        score:1
-      },
-      {
-        name:"Argentina",
-        score:0
-      },
-      {
-        name:"Norway",
-        score:-1
-      }
-    ]})
-    this.setState({teams2: [
-      {
-        name:"Sweden",
-        score:3
-      },
-      {
-        name:"France",
-        score:1
-      },
-      {
-        name:"Colombia",
-        score:0
-      },
-      {
-        name:"China",
-        score:-1
-      }
-    ]})
+    var A = '[{"name":"Brazil","score":3},{"name":"Germany","score":1},{"name":"Argentina","score":0},{"name":"Norway","score":-1}]';
+    var B = '[{"name":"Div","score":3},{"name":"Germany","score":1},{"name":"Argentina","score":0},{"name":"Norway","score":-1}]';
+    var C = '[{"name":"Face","score":3},{"name":"Germany","score":1},{"name":"Argentina","score":0},{"name":"Norway","score":-1}]';
+    var D = '[{"name":"Lol","score":3},{"name":"Germany","score":1},{"name":"Argentina","score":0},{"name":"Norway","score":-1}]';
 
-}
+    this.setState(
+      {
+        groupA: JSON.parse(A),
+        groupB: JSON.parse(B),
+        groupC: JSON.parse(C),
+        groupD: JSON.parse(D)
+
+      }
+    );
+  }
+
+
 
 render(){
+    const r = JSON.stringify(this.state.teams);
+    console.log(r);
+
     return (
       <div className="App">
-        <Group groupLetter = "A" teams={this.state.teams}/>
+        <Group groupLetter = "A" teams={this.state.groupA}/>
         <br/>
-        <Group groupLetter = "B" teams={this.state.teams2}/>
+        <Group groupLetter = "B" teams={this.state.groupB}/>
+        <br/>
+        <Group groupLetter = "C" teams={this.state.groupC}/>
+        <br/>
+        <Group groupLetter = "D" teams={this.state.groupD}/>
+        <br/>
 
       </div>
     );
